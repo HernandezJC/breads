6bcb48f1235bad98aec761919fdf8ca0e2ba8396
HernandezJC <jchernandez1021@gmail.com>
2023-06-05 20:55:39 -0700

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -15,6 +15,14 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
@@ -25,6 +33,23 @@ breads.get('/:arrayIndex', (req, res) => {
   } else {
     res.send('404')
   }
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
 })
 
 // CREATE
@@ -41,10 +66,5 @@ breads.post('/', express.urlencoded({ extended: true }), (req, res) => {
   res.redirect('/breads')
 })
 
-// DELETE
-breads.delete('/:indexArray', (req, res) => {
-  Bread.splice(req.params.indexArray, 1)
-  res.status(303).redirect('/breads')
-})
 
 module.exports = breads
--- views/show.jsx
+++ views/show.jsx
@@ -21,6 +21,8 @@ function Show ({ bread, index }) {
       
       <form action={`/breads/${index}?_method=DELETE`} method="POST">
         <input type='submit' value="DELETE"/>
+        <br />
+        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
       </form>
         </Default>      
       );
