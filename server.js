8254e6e43984f6045b8045faed160770f8d2b20b
HernandezJC <jchernandez1021@gmail.com>
2023-06-24 10:37:35 -0700

    updated server.js

--- server.js
+++ server.js
@@ -1,87 +1,89 @@
-// // DEPENDENCIES
 // const express = require('express')
 // const app = express()
 // const methodOverride = require('method-override')
+// const mongoose =require('mongoose')
 
-// // CONFIGURATION
+// //configuration:
 // require('dotenv').config()
 // const PORT = process.env.PORT
 // console.log(PORT)
 
-// //Middleware
-// const breadsController = require('./controllers/breads_controller.js')
-// const breads = require('./controllers/breads_controller.js')
+// //routes:
+// app.get('/', (req, res) => {
+//     res.send('Welcome to the World of Bread!')
+// })
+
+// mongoose.connect(process.env.MONGO_URI, 
+//     {useNewUrlParser: true, useUnifiedTopology: true},()=>{
+//         console.log('Connected to MongoDB:', process.env.MONGO_URI)
+// })
+
+  
+// //middleware:
 // app.use(methodOverride('_method'))
-// app.use('/breads', breadsController)
 // app.use(express.static('public'))
-// app.use(express.urlencoded({extended: true}))
+// app.use(express.urlencoded({extended:true}))
 // app.set('views', __dirname + '/views')
 // app.set('view engine', 'jsx')
 // app.engine('jsx', require('express-react-views').createEngine())
 
-// // ROUTES
-// app.get('/', (req, res) => {
-//   res.send('Welcome to an Awesome App about Breads!')
-// })
-
-// // LISTEN
-// app.listen(PORT, () => {
-//   console.log('listening on port:', PORT);
-// })
+// //Breads
+// const breadsController = require('./controllers/breads_controller.js')
+// app.use('/breads', breadsController)
+ 
+// //Bakers
+// const bakersController = require('./controllers/bakers_controller.js')
+// app.use('/bakers', bakersController)
 
 // // 404 Page
 // app.get('*', (req, res) => {
-//   res.send('404')
+//     res.send('404')
+//   })
+  
+
+
+// //Adding Comment Checking Git
+
+// app.listen(PORT, ()=> {
+//     console.log('listening on port: ', PORT)
 // })
 
-
+//Henry Code
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
-const mongoose =require('mongoose')
-
-//configuration:
+const mongoose = require('mongoose')
+// CONFIGURATION
 require('dotenv').config()
-const PORT = process.env.PORT
-console.log(PORT)
 
-//routes:
-app.get('/', (req, res) => {
-    res.send('Welcome to the World of Bread!')
+app.get('/', (req,res) =>{
+    res.send('Welcome to an Awesome Bread App')
 })
 
-mongoose.connect(process.env.MONGO_URI, 
-    {useNewUrlParser: true, useUnifiedTopology: true},()=>{
-        console.log('Connected to MongoDB:', process.env.MONGO_URI)
-})
 
-  
-//middleware:
-app.use(methodOverride('_method'))
-app.use(express.static('public'))
-app.use(express.urlencoded({extended:true}))
+// MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
 
 //Breads
-const breadsController = require('./controllers/breads_controller.js')
+const breadsController = require('./controllers/breads_controller')
 app.use('/breads', breadsController)
- 
-//Bakers
-const bakersController = require('./controllers/bakers_controller.js')
+
+//BAKERS
+const bakersController = require('./controllers/bakers_controller')
 app.use('/bakers', bakersController)
 
+const PORT = process.env.PORT || 3003
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+    () => {console.log('connected to : ', process.env.MONGO_URI)}
+    )
 // 404 Page
 app.get('*', (req, res) => {
     res.send('404')
   })
-  
 
-
-//Adding Comment Checking Git
-
-app.listen(PORT, ()=> {
-    console.log('listening on port: ', PORT)
-})
-// https://digitalskills.instructure.com/courses/9255/pages/code-along-be-3-server-side-views-and-rendering-with-jsx?module_item_id=1307340#
+app.listen(process.env.PORT)
